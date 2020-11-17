c95ad3383ee2aea3dd2c2644e6aa415daf55bd96
Godd-Noodle <patrickdmcgurnaghan@gmail.com>
2020-11-17 21:14:22 +0000

    semi-working faceit verify

--- Pug Maker Discord/run.py
+++ Pug Maker Discord/run.py
@@ -22,7 +22,7 @@ TOKEN = str(creds_discord["TOKEN"])
 PREFIX = str(creds_discord["PREFIX"])
 
 
-
+url_start = "https://faceitstats.com/player,"
 steam_pre = 'http://steamcommunity.com/id/'
 
 bot = commands.Bot(command_prefix=PREFIX)
@@ -38,6 +38,52 @@ async def respond(ctx):
 async def close(ctx):
 
     await bot.close()
+
+@bot.command(name = "vfaceit", pass_context=True)
+async def vfaceit(ctx,arg):
+    
+    webpage = "https://faceitstats.com/player,"
+    response = urllib.request.urlopen(webpage +arg)
+    
+    webcontentfs = str(response.read())
+
+    index = webcontentfs.find("https://steamcommunity.com/profiles/")
+
+
+    if (index == -1):
+        await ctx.send("This isnt a faceit profile")
+        return
+    i_e = webcontentfs[index:].find('"')
+
+    steamurl = webcontentfs[index:index+i_e]
+
+    response = urllib.request.urlopen(steamurl)
+    
+    webcontentsteam = str(response.read())
+    
+    
+    
+    p_s = webcontentsteam.find(str('<div class="profile_summary">'))
+    p_e = webcontentsteam.find(str('<div class="profile_summary_footer">'))
+
+
+    level_s = webcontentfs.find("level: <strong>")
+
+    level = webcontentfs[level_s:level_s+20]
+
+    level_filter = filter(str.isdigit, level)
+    faceitLevel = "".join(level_filter)
+    found = webcontentsteam[p_s:p_e].find(str(ctx.author))
+
+    if (found == -1):
+        await ctx.send(f"could not find your discord id '{ctx.author}'on this steam profile, ensure it has this so we can verify that it is your account we are linking to your faceit")
+        return
+    await ctx.send(f"index of found is {found}, faceit level is {faceitLevel}")
+    role = discord.utils.get(ctx.author.guild.roles, name = "Linked")
+    await ctx.author.add_roles(role)
+    role = discord.utils.get(ctx.author.guild.roles, name = str(faceitLevel))
+    await ctx.author.add_roles(role)
+    print(faceitLevel)  
 
 @bot.command(name = "verify", pass_context=True)
 async def verify(ctx, arg):
